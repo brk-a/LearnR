169a1e7eb33373d0bbbd5a5d6279f243e4db6675
brk-a <fnjakai@outlook.com>
2022-04-30 04:05:33 -0700

    4-tidyverse_basics: re-coding and creating new variables

--- 4-tidyverse_basics.r
+++ 4-tidyverse_basics.r
@@ -9,8 +9,8 @@
 #pkg tidyverse is a coherent system of packages for data manipulation,
 #exploration and visualisation that share a common design philosophy
 
-#use a data-set that is built into R; the star wars data-set
-# use `data()` to see the data-sets that  are available
+#use a data-set that is built into tidyverse; the star wars data-set
+# use `data()` to see the data-sets that  are available in R
 
 if (!require(tidyverse)) install.packages("tidyverse", dependencies=T)
 library(tidyverse)
@@ -25,3 +25,24 @@ starwars %>%
 	 mutate(BMI = mass / height^2) %>%
 	 group_by(gender) %>%
 	 summarise(Average_BMI = mean(BMI))
+
+sw_0 <- starwars %>%
+   select(name, height, mass, gender, everything()) %>%
+   rename(weight = mass) %>%
+   na.omit() %>%
+   mutate(height /= 100) %>%
+   mutate(BMI = weight / weight^2) %>%
+   groupby(gender) %>%
+   summarise(Average_BMI = mean(BMI))
+
+sw_1 <- starwars %>%
+     select(name, height, mass, gender) %>%
+     na.omit() %>%
+     mutate(height /= 100) %>%
+     filter(gender %in% c("male", "female")) %>%
+     mutate(gender = recode(gender, male = "m", female = "f")) %>%
+     mutate(size = height > 1 & mass > 75, if_else(
+     		 size, "big", "small")) %>%
+	mutate(BMI = mass / height^2) %>%
+	groupby(gender) %>%
+	summarise(Average_BMI = mean(BMI))
