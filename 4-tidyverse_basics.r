a215168aee1ca73ac64e4cb8fe83cbaba704c6ee
brk-a <fnjakai@outlook.com>
2022-05-01 03:18:02 -0700

    4-tidyverse_basics: subset data

--- 4-tidyverse_basics.r
+++ 4-tidyverse_basics.r
@@ -17,6 +17,8 @@ library(tidyverse)
 
 View(starwars)
 
+#### ------- 1. pipe operator `%>%` and select()  -----------------
+
 starwars %>%
 	 select(height, mass, gender, species) %>%
 	 filter(species == "Human") %>%
@@ -26,6 +28,8 @@ starwars %>%
 	 group_by(gender) %>%
 	 summarise(Average_BMI = mean(BMI))
 
+#### ------- 2. everything() ---------------------------------------
+
 sw_0 <- starwars %>%
    select(name, height, mass, gender, everything()) %>%
    rename(weight = mass) %>%
@@ -34,6 +38,8 @@ sw_0 <- starwars %>%
    mutate(BMI = weight / weight^2) %>%
    groupby(gender) %>%
    summarise(Average_BMI = mean(BMI))
+
+#### ------- 3. recode() and if_else() -----------------------------
 
 sw_1 <- starwars %>%
      select(name, height, mass, gender) %>%
@@ -46,3 +52,50 @@ sw_1 <- starwars %>%
 	mutate(BMI = mass / height^2) %>%
 	groupby(gender) %>%
 	summarise(Average_BMI = mean(BMI))
+
+#### 4. subset using filter() --------------------------------------
+
+#use the data-set `msleep`
+
+View(msleep)
+
+ms_0 <- msleep %>%
+     select(name, sleep_total) %>%
+     filter(sleep_total > 18)
+
+ms_1 <- msleep %>%
+     select(name, sleep_total) %>%
+     filter(!sleep_total > 18)
+
+ms_2 <- msleep %>%
+     select(name, order, bodywt, sleep_total) %>%
+     filter(order == "Primates", bodywt > 20)
+
+ms_3 <- msleep %>%
+     select(name, order, bodywt, sleep_total) %>%
+     filter(order == "Primates" | bodywt > 20)
+
+ms_4 <- msleep %>%
+    select(name, sleep_total) %>%
+    filter(name == "Goat" | name == "Cow" |
+    name == "Dog")
+
+ms_5 <- msleep %>%
+    select(name, sleep_total) %>%
+    filter(name %in% c("Goat", "Cow", "Dog"))
+
+ms_6 <- msleep %>%
+    select(name, sleep_total) %>%
+    filter(between(sleep_total, 16, 18))
+
+ms_7 <- msleep %>%
+    select(name, sleep_total) %>%
+    filter(near(sleep_total, 17, tol = 0.5))
+
+ms_8 <- msleep %>%
+    select(name, conservation, sleep_total) %>%
+    filter(is.na(conservation))
+
+ms_9 <- msleep %>%
+    select(name, conservation, sleep_total) %>%
+    filter(!is.na(conservation))
